52ccb2eb4ff07d3bffbf20290847cc32d0428cab
E5c11 <emmanuelconradie@gmail.com>
2022-10-31 23:15:26 +0200

    Initial commit

---
+++ app/src/main/java/com/esc/workmood/ui/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.esc.workmood.ui.home
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.esc.workmood.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+        Log.d("myT", "Home: ")
+    }
+
+}
